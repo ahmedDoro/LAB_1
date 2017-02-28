149654b510aa31310bcd7cd9df6dc4226b2c1b83
1600574 <a@a.com>
2017-02-28 15:58:08 +0000

    update

--- home.php
+++ home.php
@@ -1,10 +1,11 @@
-<!DOCTYPE html> 
+
 <html> 
 <head> 
     <title>selfreference</title> 
 </head> 
 <body> 
-<?php  if ($_SERVER['REQUEST_METHOD'] === 'GET') { 
+<?php 
+if ($_SERVER['REQUEST_METHOD'] === 'GET') { 
     echo "Show something";//execute if requested using HTTP GET Method 
 }
     elseif ($_SERVER['REQUEST_METHOD'] === 'POST') { 
