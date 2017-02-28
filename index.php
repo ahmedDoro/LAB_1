6d488d10fa37ace45c48007efa9dea7cd7679175
1600574 <a@a.com>
2017-02-28 15:49:15 +0000

    update

--- index.php
+++ index.php
@@ -1,31 +1,17 @@
 
-<!DOCTYPE html>
-  <html> 
-    <head> 
-        <title>selfreference</title> 
-    </head> 
-    <body> 
-    <?php
-        if($_SERVER['REQUEST_METHOD']== 'GET'){
-        ?>
-         <form action=“<? echo "$_SERVER[‘PHP_SELF']"; ?>" method="post"> 
-             <label>Forename</label> 
-             <input type="text" name="forename"> 
-             <label>Surname</label> 
-             <input type="text" name="surname"> 
-             <p><input type="submit" value="Submit"></p> 
-         </form> 
-        <?
-         }
-        elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
-             
-            // execute if requested using HTTP POST Method 
-            $forename = $_POST["forename"];
-             
-            $surname = $_POST["surname"];
-             
-            print("<h1>Hello {$forename} {$surname}</h1>");
-             
-        }
-
-?>
+<!DOCTYPE html> 
+<html> 
+<head> 
+    <title>selfreference</title> 
+</head> 
+<body> 
+    <?php  if ($_SERVER['REQUEST_METHOD'] === 'GET') { 
+//execute if requested using HTTP GET Method 
+    }
+    elseif ($_SERVER['REQUEST_METHOD'] === 'POST') { 
+    // execute if requested using HTTP POST Method  } 
+     else { 
+    // this is impossible  } 
+     ?> 
+</body> 
+</html>
