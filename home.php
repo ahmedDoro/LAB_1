7aca9af3df9b1ce1d778fbeff1717af3ee789d62
1600574 <a@a.com>
2017-03-07 15:48:53 +0000

    update

--- home.php
+++ home.php
@@ -1,17 +1,19 @@
 
 <html> 
 <head> 
-    <title>selfreference</title> 
+    <title>login system</title> 
 </head> 
 <body> 
 <?php 
-if ($_SERVER["REQUEST_METHOD"] === 'GET') { 
+
+    if($_SERVER["REQUEST_METHOD"] === 'GET') { 
     echo "Show something"; //execute if requested using HTTP GET Method 
-}
+    }
     elseif ($_SERVER["REQUEST_METHOD"] === 'POST'){
      
     echo "Show something worked";
 }
+
      else { 
      echo "Show something please";
      ?> 
